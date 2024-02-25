40a69522060f9b0050cee5985ff1d3c156934c01
Aashima24 <151027863+Aashima24@users.noreply.github.com>
2024-02-25 16:24:29 +0000

    config

---
+++ 01_basics/conversion.js
@@ -0,0 +1,12 @@
+// let score="90jsbf"
+// console.log(typeof score);
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+// let isLoggenIn=1
+// let booleanisLoggedin=Boolean(isLoggenIn);
+// console.log(booleanisLoggedin)
+let someNumber=33
+let stringNumber=String(someNumber);
+console.log(typeof(stringNumber));
+console.log(stringNumber)
