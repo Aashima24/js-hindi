c6cc5e61ce344c47ebadc1628b550bceebe0567f
Aashima24 <151027863+Aashima24@users.noreply.github.com>
2024-02-25 16:09:35 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=14453;
+let accountEmail="aashimagarg24@gmail.com";
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState
+// accountId=2
+ console.log(accountPassword);
+ accountEmail="hello@12234"
+ accountPassword="saibaba@123"
+ accountCity="BAngagalru"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+// "use strict";
+// alert("hello") 
+// we are using node js not browser
+//reading ability shuld be high
+let age=90
+let name="aashima"
+let flag=yes
+let followr;
+console.log(typeof null);
