564d70eb9200195120ff791ea0f09c260436a94b
Aashima24 <151027863+Aashima24@users.noreply.github.com>
2024-02-27 09:05:21 +0000

    math_num

---
+++ 01_basics/nums_maths.js
@@ -0,0 +1,24 @@
+// let a=300;
+// console.log(a)
+// let score=new Number(399)
+// console.log(score)
+// console.log(typeof score.toString())
+// console.log(a.toFixed(2))
+// let num2=1887.37353
+// console.log(num2.toPrecision(3))
+// let num3=10000000
+// console.log(num3.toLocaleString('en-IN'))
+
+
+// ++++++++++++++++MATHS+++++++++++++++++
+
+// console.log(Math.round(98.783219))
+// console.log(Math.abs(-3278))
+// console.log(Math.floor(68.24))
+// console.log(Math.ceil(68.24))
+// console.log(Math.min(12,3,5,73))
+console.log((Math.random()*10)+1)
+console.log((Math.random()))
+let min=10
+let max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
---
+++ 01_basics/strings.js
@@ -0,0 +1,18 @@
+let name="aashima Garg is a best software developer"
+console.log(name.length);
+// console.log(name[2]);
+// let bas=name;
+// console.log(bas);
+// bas="babu"
+// console.log(bas.toUpperCase());
+// console.log(name.indexOf('z'));
+// console.log(name.substring(0,4))
+// let anotherstring =name.slice(2);
+// console.log(anotherstring)
+let string1="   aash   "
+console.log(string1.trim())
+let url="https://aashima20%@google.com"
+console.log(url.replace('20%','-'))
+console.log(url.includes('aasz'))
+console.log(name.split())
+console.log(name.split(' '))
