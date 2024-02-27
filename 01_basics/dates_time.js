44d1a3c854f383f51ca42ba4d3cb64c1c893de12
Aashima24 <151027863+Aashima24@users.noreply.github.com>
2024-02-27 09:49:41 +0000

    day and time

---
+++ 01_basics/dates_time.js
@@ -0,0 +1,19 @@
+let myDate=new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(new Date().toDateString());
+// console.log(myDate.toLocaleString());
+
+// let mycreatedDate=new Date(2012,2,24)
+// console.log(mycreatedDate.toDateString())
+// let datenew=Date.now()
+// console.log(datenew);
+// console.log(Math.floor(Date.now()/1000))
+
+let todayDate=new Date()
+console.log(todayDate)
+console.log(todayDate.getMonth())
+console.log(todayDate.getDay())
