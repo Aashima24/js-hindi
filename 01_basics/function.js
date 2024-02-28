c553b2147bbb578cca275b8715de0c08a6691331
Aashima24 <151027863+Aashima24@users.noreply.github.com>
2024-02-28 11:24:07 +0000

    func

---
+++ 01_basics/function.js
@@ -0,0 +1,16 @@
+const chai=function(a,b)
+{
+    console.log(a+b);
+}
+chai(3,4)
+ function cha(a,b){
+    console.log(this)
+    return a*b;
+ }
+ console.log(cha(23,43))
+ const chaaa=(a,b)=>{
+    let username="jd"
+     console.log(a-b)
+     console.log(this.username)
+ }
+ chaaa(12,2)
